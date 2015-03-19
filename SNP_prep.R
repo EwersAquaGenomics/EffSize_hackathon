119fe387a3c1b4a176bc0e3f6182ba5063712b39
Christine Ewers-Saucedo <ewers.christine@gmail.com>
2015-03-19 14:20:18 -0400

    ongoing work

---
+++ SNP_prep.R
@@ -0,0 +1,12 @@
+# Workflow for SNP data
+setwd("/Users/christineewers/Desktop")
+
+# import SNP data
+snp.genlight <- read.snp()
+
+# calculate coalescent Ne
+# input: phylo object
+# get trees from BEAST or SNPphylo
+
+# calculate other Ne estimates
+# input: genlight object snp.genlight
