d38f8f2de03f6901397a21394732b813859f7c5d
RBHarris <rbharris@uw.edu>
2015-03-19 14:51:30 -0400

    The ms2ms function in R. Original script written by Pidugu and Schlotterer (2006) in PERL.

---
+++ ms2ms.R
@@ -0,0 +1,55 @@
+#Example ms string: ms.output <- ms(nsam = 10, nreps = 5, opts = "-t 10 -I 2 6 4 2 -ej 0.2 2 1")
+
+ms2ms <- function(ms.output){
+	cmd <- strsplit(ms.output[1], " ")[[1]]
+	nsam <- as.numeric(cmd[2])
+	nreps <- as.numeric(cmd[3])
+	find.I <- which(cmd == "-I")
+	# Did you specify more than one population?
+	if (length(find.I) != 0 & cmd[find.I + 1] > 1) {
+		n.pops <- as.numeric(cmd[find.I + 1])
+		n.ind.p <- as.numeric(cmd[(find.I + 2):(find.I + n.pops + 1)])
+		if (!any(n.ind.p %% 2 == 0)) {
+			print("The number of individuals in each population must be even.")
+			return(NULL)
+		} else {
+			ind.nam <- NULL
+			for (np in 1:n.pops){
+				ind.nam <- c(ind.nam, paste("Ind", c(1:(n.ind.p[np]/2)), ".p", np, sep = ""))
+			}
+		}
+	} else {
+		ind.nam <- paste("Ind", c(1:(nsam/2)), ".p1", sep = "")
+	}
+	pos <- grep("positions", ms.output)
+	full.mat <- NULL
+	for (i in 1: nreps){
+		loci <- ms.output[(pos[i]+1):c(pos[i]+ nsam)]
+		n.pos <- nchar(loci[i])
+		mut <- sample(c("+", "-"), n.pos, replace = TRUE)
+		val.vec <- rep(0, nsam)
+		for (j in 1: nsam){
+			locus <- strsplit(loci[j], "")[[1]]
+			val <- 100
+			for (k in 1:n.pos){
+				if (mut[k] == "+" & locus[k] == 1) val <- val + 1
+				if (mut[k] == "-" & locus[k] == 1) val <- val - 1
+			}
+			val.vec[j] <- val
+		}	
+		alleles <- unique(val.vec)
+		al.mat <- matrix(0, nrow = nsam/2, ncol = length(alleles))
+		colnames(al.mat) <- paste("L", i, ".", c(1:length(alleles)), sep = "")
+		for (ii in 1:(nsam/2)){
+			if (ii == 1) jj <- ii
+			else jj <- jj + 2
+			al.mat[ii, which(alleles == val.vec[jj])] <- sum(0.5, al.mat[ii, which(alleles == val.vec[jj])])
+			al.mat[ii, which(alleles == val.vec[jj+1])] <- sum(0.5, al.mat[ii, which(alleles == val.vec[jj+1])])
+		}
+		full.mat <- cbind(full.mat, al.mat)
+		rownames(full.mat) <- ind.nam
+	}
+	full.mat <- genind(tab = full.mat, pop = as.numeric(unlist(strsplit(ind.nam, ".p"))[seq(2, nsam, 2)]))
+	return(full.mat)
+}
+ms2ms(ms.output)@tab
