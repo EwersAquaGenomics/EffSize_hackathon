38f985b2c0df80f6501d23707066f4853f8b363c
Julia Palacios <akshaywalia@tests-MacBook-Pro-3.local>
2015-03-19 12:33:12 -0400

    hiden functions

--- simulate_trees.R
+++ simulate_trees.R
@@ -1,4 +1,70 @@
-coalgen_thinning_iso<-function(sample, trajectory, upper = 25, ...) {
+#'@title .generate_newick
+#'@description Generates a newick file for heterochronous trees. 
+#'@param args is a list with coal_factor, sampling and coalescent times, events and indicator. This is the output of gen_INLA_args().
+#'@param sample is a matrix or vector. The first column indicates the number of samples and the second column indicates the sampling times
+#'@author Julia Palacios \email{julia.pal.r@@gmail.com}
+#'@examples
+#'generate_newick()
+#'
+
+.generate_newick<-function (args, sample) 
+{
+  n <- sum(sample[, 1])
+  labels <- paste(rep("t", n), seq(1, n, 1), rep("_", n), rep(sample[, 
+                                                                     2], sample[, 1]), sep = "")
+  tb <- sample[1, 1]
+  s <- 0
+  temp_labels <- labels[1:tb]
+  temp_times <- rep(sample[1, 2], sample[1, 1])
+  initial.row <- 2
+  for (j in 2:length(args$event)) {
+    if (args$event[j] == 1) {
+      s <- args$s[j]
+      ra <- sample(tb, 1)
+      if (ra < tb) {
+        new_label <- paste("(", temp_labels[ra], ":", 
+                           s - temp_times[ra], ",", temp_labels[ra + 1], 
+                           ":", s - temp_times[ra + 1], ")", sep = "")
+        temp_labels[ra] <- new_label
+        temp_labels <- temp_labels[-(ra + 1)]
+        temp_times[ra] <- s
+        temp_times <- temp_times[-(ra + 1)]
+      }
+      else {
+        new_label <- paste("(", temp_labels[ra], ":", 
+                           s - temp_times[ra], ",", temp_labels[1], ":", 
+                           s - temp_times[1], ")", sep = "")
+        temp_labels[1] <- new_label
+        temp_labels <- temp_labels[-(ra)]
+        temp_times[1] <- s
+        temp_times <- temp_times[-(ra)]
+      }
+      tb <- tb - 1
+    }
+    else {
+      s <- args$s[j]
+      if (sample[initial.row, 1] == 1) {
+        temp_labels <- c(temp_labels, labels[cumsum(sample[, 
+                                                           1])[initial.row]])
+        initial.row <- initial.row + 1
+        tb <- tb + 1
+        temp_times <- c(temp_times, s)
+      }
+    }
+  }
+  out.tree <- read.tree(text = paste(temp_labels, ";", sep = ""))
+  return(list(newick = out.tree, labels = labels))
+}
+
+#'@title .coalgen_thinning_iso
+#'@description Simulates coalescent times using thinning for isochronous sampling
+#'@param sample is a matrix or vector. The first column indicates the number of samples and the second column indicates the sampling times
+#'@param trajectory is a function with the population size function
+#'@param upper is an upper value of inverse trajectory (optional)
+#'@author Julia Palacios \email{julia.pal.r@@gmail.com}
+#'@examples
+
+.coalgen_thinning_iso<-function(sample, trajectory, upper = 25, ...) {
   s = sample[2]
   n <- sample[1]
   out <- rep(0, n - 1)
@@ -19,7 +85,16 @@ coalgen_thinning_iso<-function(sample, trajectory, upper = 25, ...) {
   return(list(intercoal_times = c(out[1], diff(out)), lineages = seq(n, 2, -1),upper=upper))
 }
 
-coalgen_thinning_hetero<-function (sample, trajectory, upper = 25, ...) {
+
+#'@title .coalgen_thinning_hetero
+#'@description Simulates coalescent times using thinning for isochronous sampling
+#'@param sample is a matrix or vector. The first column indicates the number of samples and the second column indicates the sampling times
+#'@param trajectory is a function with the population size function
+#'@param upper is an upper value of inverse trajectory (optional)
+#'@author Julia Palacios \email{julia.pal.r@@gmail.com}
+#'@examples
+
+.coalgen_thinning_hetero<-function (sample, trajectory, upper = 25, ...) {
   n_sampled<-sample[,1]
   s_times<-sample[,2]
   s = sample[1, 2]
@@ -77,12 +152,20 @@ coalgen_thinning_hetero<-function (sample, trajectory, upper = 25, ...) {
   intercoal_times = c(out[1], diff(out))
   lineages = branches
   coal_times<-cumsum(intercoal_times)
-  args<-gen_INLA_args(coal_times,s_times,n_sampled)
-  out<-generate_newick(args,cbind(n_sampled,s_times))$newick
+  args<-.gen_INLA_args(coal_times,s_times,n_sampled)
+  out<-.generate_newick(args,cbind(n_sampled,s_times))$newick
   return(out)
 }
 
-gen_INLA_args<-function (coal_times, s_times, n_sampled) {
+#'@title .gen_INLA_args
+#'@description Used for heterochronous sampling. It has nothing to do with INLA
+#'@param coal_times indicates the coalescent times
+#'@param s_times indicates the sampling times
+#'@param n_sampled the number of samples 
+#'@author Julia Palacios \email{julia.pal.r@@gmail.com}
+#'@examples
+
+.gen_INLA_args<-function (coal_times, s_times, n_sampled) {
   n = length(coal_times) + 1
   data = matrix(0, nrow = n - 1, ncol = 2)
   data[, 1] = coal_times
@@ -159,7 +242,7 @@ simulate.tree<-function(n=10,N=1,sampling="iso",args="-T -G 0.1",Ne=1,max=1,simu
       if (is.null(sample)){
         sample<-c(n,0)
       }
-      out<-replicate(N,rcoal(n,br=coalgen_thinning_iso(sample,fun_inv,max)$intercoal_times),simplify=FALSE)
+      out<-replicate(N,rcoal(n,br=.coalgen_thinning_iso(sample,fun_inv,max)$intercoal_times),simplify=FALSE)
       class(out)<-"multiPhylo"
       
       return(list(out=out,description="isochronous simulation using thinning with trajectory provided"))
@@ -171,13 +254,13 @@ simulate.tree<-function(n=10,N=1,sampling="iso",args="-T -G 0.1",Ne=1,max=1,simu
       #        #sampling is heterochronous
       if (is.null(sample)){ #it is actually isochronous
         sample<-c(n,0)
-        out<-replicate(N,rcoal(n,br=coalgen_thinning_iso(sample,fun_inv,max)$intercoal_times),simplify=FALSE)
+        out<-replicate(N,rcoal(n,br=.coalgen_thinning_iso(sample,fun_inv,max)$intercoal_times),simplify=FALSE)
         class(out)<-"multiPhylo"
         
         return(list(out=out,description="isochronous simulation using thinning with trajectory provided"))
       }else{
         #It is indeed heterochronous
-        out<-replicate(N,coalgen_thinning_hetero(sample, fun_inv,max),simplify=FALSE)     
+        out<-replicate(N,.coalgen_thinning_hetero(sample, fun_inv,max),simplify=FALSE)     
         class(out)<-"multiPhylo" 
         return(list(out=out,description="Simulation with thinning for heterochronous coalescent with a specific function"))
         #   
